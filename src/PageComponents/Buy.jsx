28936ff6ccb32cce5b98878586bf47a8d6e563ca
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-10-16 18:44:20 -0400

    good place to stop

--- src/PageComponents/Buy.jsx
+++ src/PageComponents/Buy.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const Buy = (props) =>{
   console.log(props.location.state);
   return(
-    <h1>Buy Page</h1>
+    <div>
+      <h1>Buy Page</h1>
+      <Link to='/Rates'>
+        Back
+      </Link>
+    </div>
   );
 };
 
--- src/PageComponents/Rates.jsx
+++ src/PageComponents/Rates.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import Input from '../FormComponents/Input';
 
 const Rates = (props) =>{
   const {name, state}=props.location.state;
@@ -7,6 +8,7 @@ const Rates = (props) =>{
     <div>
       <h1>Rates Page</h1>
       <h3>{name} is from {state}</h3>
+      <Input placeholder={'Something to add'} handleChange={props.handleChange}/>
       <Link to='/'>
         Back
       </Link>
