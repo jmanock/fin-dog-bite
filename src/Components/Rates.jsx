0aa3f9c5ad758117eddc5c48b6542f213cbada72
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-23 07:43:19 -0500

    setting up ratesSection

--- src/Components/Rates.jsx
+++ src/Components/Rates.jsx
@@ -4,7 +4,23 @@ class Rates extends Component{
   render(){
     console.log(this.props.state.dogs, this.props.state.addIns);
     return(
-      <h1>Rates Section</h1>
+      <div className='container'>
+        <h3>Please Choose your coverage Limit, Premium for your Annual Policy</h3>
+        <small>NOTE: Installments are always billed on the 3rd day of the month</small>
+        <table>
+          <thead>
+            <tr>
+              <th>Coverage Limit</th>
+              <th>Total</th>
+              <th># Installments</th>
+              <th>Down Payment</th>
+              <th>Installment Amount</th>
+              <th>First Bill</th>
+              <th>Select Coverage</th>
+            </tr>
+          </thead>
+        </table>
+      </div>
     )
   }
 }
