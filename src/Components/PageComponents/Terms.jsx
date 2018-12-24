4a3a55ac32301312dffa9c61e4045f492e1381f0
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-24 11:42:08 -0500

    working on terms page

--- src/Components/PageComponents/Terms.jsx
+++ src/Components/PageComponents/Terms.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
 const Terms = () => (
-  <h1>Terms Page</h1>
+  <div className='container'>
+    <h3 className='sectionTitle'>Terms &amp; Conditions</h3>
+    <div className='card'>
+      <div className='card-body'>
+        <p className='card-text'>
+          <span className='title-name'>Canine Liability</span> offers this Website, including all information, software, products and services available from this website or offered as part of or in conjunction with this Website (the "Website"), to you, the user, conditioned upon your acceptance of all of the terms, conditions, policies and notices stated here.
+        </p>
+        <p className='card-text'>
+          <span className='title-name'>Canine Liability</span> reserves the right to make changes to these Terms and Conditions immediately by posting the changed Terms and Conditions in this location.
+        </p>
+        <p className='card-text'>
+          Your continued use of the Website constitutes your agreement to all such terms, conditions and notices, and any changes to the Terms and Conditions made by <span className='title-name'>Canine Liability</span>.
+        </p>
+        <p className='card-text'>
+          The term `http://www.dogbitequote.com` or `us` or `we` refers to the owner of the website. The term `you` refers to the user or viewer of our website.
+        </p>
+      </div>
+    </div>
+
+    <div className='card'>
+      <div className='card-body'>
+        <h4 className='card-title'>The use of this website is subject to the following terms of use:</h4>
+        <p className='card-text'>
+          Use the website at your own risk. This website is provided to you "as is," without warranty of any kind either express or implied. Neither <span className='title-name'>Canine Liability</span> nor its employees, agents, third-party information providers, merchants, licensors or the like warrant that the Website or its operation will be accurate, reliable, uninterrupted or error-free. No agent or representative has the authority to create any warranty regarding the Website on behalf of <span className='title-name'>Canine Liability</span>. <span className='title-name'>Caine Liability</span> reserves the right to change or discontinue at any time any aspect or feature of the Website.
+        </p>
+      </div>
+    </div>
+
+    <div className='card'>
+      <div className='card-body'>
+        <h4 className='card-title'>Exclusion of Liability</h4>
+        <p className='card-text'>
+          The content of the pages of this website is for your general information and use only. It is subject to change without notice.
+        </p>
+        <p className='card-text'>
+          Neither we nor any third parties provide any warranty or guarantee as to the accuracy, timeliness, performance, completeness or suitability of the information and materials found or offered on this website for any particular purpose. You acknowledge that such information and materials may contain inaccuracies or errors and we expressly exclude liability for any such inaccuracies or errors to the fullest extent permitted by law.
+        </p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Terms;
