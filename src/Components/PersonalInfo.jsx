61fd0cb0b0182187ad20f5b82e12f11adea04882
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2018-12-29 10:14:26 -0500

    added personal questions

--- src/Components/PersonalInfo.jsx
+++ src/Components/PersonalInfo.jsx
@@ -1,11 +1,15 @@
 import React,{Component} from 'react';
 import Input from './FormComponents/Input';
+import Select from './FormComponents/Select';
+import States from './OptionsComponents/State';
+import Button from './FormComponents/Button';
 
 class PersonalInfo extends Component{
   state = {
     personalInfo:{
-      fName:'',lName:'',date:'',fNameSpouse:'',lNameSpouse:'',address:'',city:'',state:'',zip:'',dogMailingAddress:'',phone:'',fax:''
-    }
+      fName:'',lName:'',date:'',fNameSpouse:'',lNameSpouse:'',address:'',city:'',state:'',zip:'',dogMailingAddress:'',phoneNumber:'',faxNumber:''
+    },
+    stateOptions:States
   }
   onInputChange = (e) =>{
     const newPerson = this.state.personalInfo;
@@ -18,19 +22,29 @@ class PersonalInfo extends Component{
     console.log(this.state);
     return(
       <form>
-        <Input type={'text'} title={'First Name'} name={'fName'} value={this.state.personalInfo.fName} placeholder={"Applicant's First Name:"} handleChange={this.onInputChange}/>
+        <Input type={'text'} title={'First Name'} name={'fName'} value={this.state.personalInfo.fName} placeholder={"Enter first name:"} handleChange={this.onInputChange}/>
 
-        <Input type={'text'} title={'Last Name'} name={'lName'} value={this.state.personalInfo.lName} placeholder={"Applicant's Last Name:"} handleChange={this.onInputChange} />
+        <Input type={'text'} title={'Last Name'} name={'lName'} value={this.state.personalInfo.lName} placeholder={"Enter last name:"} handleChange={this.onInputChange} />
 
-        <Input type={'text'} title={'Fist Name of your spouse, signifigant other or roommate:'} name={'fNameSpouse'} value={this.state.personalInfo.fNameSpouse} placeholder={'First Name of your spouse, signifigant other or roommate:'} handleChange={this.onInputChange} />
+        <Input type={'text'} title={'Fist Name of your spouse, signifigant other or roommate:'} name={'fNameSpouse'} value={this.state.personalInfo.fNameSpouse} placeholder={'Enter first name:'} handleChange={this.onInputChange} />
 
-        <Input type={'text'} title={'Last Name of your spouse, signifigant other or roommate:'} name={'lNameSpouse'} value={this.state.personalInfo.lNameSpouse} placeholder={'Last Name of your spouse, sigifigant other or roommate:'} handleChange={this.onInputChange} />
+        <Input type={'text'} title={'Last Name of your spouse, signifigant other or roommate:'} name={'lNameSpouse'} value={this.state.personalInfo.lNameSpouse} placeholder={'Enter last name:'} handleChange={this.onInputChange} />
 
-        <Input type={'text'} title={"Applicant's mailing address:"} name={'address'} value={this.state.personalInfo.address} placeholder={"Applicant's mailing address:"} handleChange={this.onInputChange} />
+        <Input type={'text'} title={"Applicant's mailing address:"} name={'address'} value={this.state.personalInfo.address} placeholder={"Enter applicant's mailing address:"} handleChange={this.onInputChange} />
 
-        <Input type={'number'} title={'Zip:'} name={'zip'} value={this.state.personalInfo.zip} placeholder={'Zip:'} handleChange={this.onInputChange} />
+        <Input type={'number'} title={'Zip:'} name={'zip'} value={this.state.personalInfo.zip} placeholder={'Enter zip:'} handleChange={this.onInputChange} />
 
-        
+        <Input type={'text'} title={'City:'} name={'city'} value={this.state.personalInfo.city} placeholder={'Enter city'} handleChange={this.onInputChange} />
+
+        <Select title={'State:'} name={'state'} options={this.state.stateOptions} value={this.state.personalInfo.state} placeholder={'Please choose...'} handleChange={this.onInputChange} />
+
+        <Select title={'Do your dog(s) live at the mailing address:'} name={'dogMailingAddress'} value={this.state.personalInfo.dogMailingAddress} options={['Yes','No']} placeholder={'Please choose...'} handleChange={this.onInputChange} />
+
+        <Input type={'number'} title={'Phone Number:'} value={this.state.personalInfo.phoneNumber} placeholder={'Enter phone number'} handleChange={this.onInputChange} />
+
+        <Input type={'number'} title={'Fax Number:'} value={this.state.personalInfo.faxNumber} placeholder={'Enter fax number'} handleChange={this.onInputChange} />
+
+        <Button type={'btn btn-success'} title={'Next'} className={'fa fa-paw'} />
       </form>
     );
   }
