888984c8c41457e1fb446bee3ec630ddc4aa75a1
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-01-04 09:16:42 -0500

    form shows up need to see if I can use it

--- src/Components/InsuredInfo.jsx
+++ src/Components/InsuredInfo.jsx
@@ -15,14 +15,9 @@ class InsuredInfo extends Component{
   }
 
   componentWillReceiveProps(nextProps){
-    //  newForm = nextProps.state.addIns.map((x) =>{
-    //   return(
-    //     <div key={x.id}>
-    //       <p>{x.id}</p>
-    //       <p>{x.interestName}</p>
-    //     </div>
-    //   )
-    // })
+    this.setState({
+      additionalIns:nextProps.state.addIns
+    });
   }
 
   onInputChange = (e) =>{
@@ -34,40 +29,39 @@ class InsuredInfo extends Component{
   }
 
   render(){
-    let newForm;
-    if(this.props.state.addIns > '0'){
-      newForm = this.props.state.addIns.map((x) =>{
+    let knew;
+    if(this.state.additionalIns.length > 0){
+      knew = this.state.additionalIns.map((x) =>{
         return(
-          <div key={x.id}>{x.id + 1}
+          <div key={x.id}>
+            <span style={{color:'red'}}>{x.id+1}</span>
             <Input type={'text'} title={'Interes Name:'} name={'interestName'} value={this.state.additionalIns.interestName} placeholder={'Enter interest name:'} handleChange={this.onInputChange} />
 
-            <Select title={'Relationship:'} name={'relationship'} options={['Landlord', 'Employer','Governmental Entity','Other']} value={this.state.additionalIns.relationship} placeholder={'Please choose...'} handleChange={this.onInputChange} />
+           <Select title={'Relationship:'} name={'relationship'} options={['Landlord', 'Employer','Governmental Entity','Other']} value={this.state.additionalIns.relationship} placeholder={'Please choose...'} handleChange={this.onInputChange} />
 
-            <Input type={'text'} title={'Address:'} name={'address'} value={this.state.additionalIns.address} placeholder={'Enter address:'} handleChange={this.onInputChange} />
+           <Input type={'text'} title={'Address:'} name={'address'} value={this.state.additionalIns.address} placeholder={'Enter address:'} handleChange={this.onInputChange} />
 
-            <Input type={'text'} title={'City:'} name={'city'} value={this.state.additionalIns.city} placeholder={'Enter city:'} handleChange={this.onInputChange} />
+           <Input type={'text'} title={'City:'} name={'city'} value={this.state.additionalIns.city} placeholder={'Enter city:'} handleChange={this.onInputChange} />
 
-            <Select title={'State:'} name={'state'} options={this.state.options.state} value={this.state.additionalIns.state} placeholder={'Please choose...'} handleChange={this.onInputChange} />
+           <Select title={'State:'} name={'state'} options={this.state.options.state} value={this.state.additionalIns.state} placeholder={'Please choose...'} handleChange={this.onInputChange} />
 
-            <Input type={'number'} title={'Zip:'} value={this.state.additionalIns.zip} placeholder={'Enter zip:'} handleChange={this.onInputChange} />
+           <Input type={'number'} title={'Zip:'} value={this.state.additionalIns.zip} placeholder={'Enter zip:'} handleChange={this.onInputChange} />
 
-            <Input type={'text'} title={'Contact First Name:'} name={'fName'} value={this.state.additionalIns.fName} placeholder={'Enter contact first name'} handleChange={this.onInputChange} />
+           <Input type={'text'} title={'Contact First Name:'} name={'fName'} value={this.state.additionalIns.fName} placeholder={'Enter contact first name'} handleChange={this.onInputChange} />
 
-            <Input type={'text'} title={'Contact Last Name:'} name={'lName'} value={this.state.additionalIns.lName} placeholder={'Enter contact last name'} handleChange={this.onInputChange} />
+           <Input type={'text'} title={'Contact Last Name:'} name={'lName'} value={this.state.additionalIns.lName} placeholder={'Enter contact last name'} handleChange={this.onInputChange} />
 
-            <Input type={'number'} title={'Contact Phone Number:'} name={'phone'} value={this.state.additionalIns.phone} placeholder={'Enter contact phone number'} handleChange={this.onInputChange} />
+           <Input type={'number'} title={'Contact Phone Number:'} name={'phone'} value={this.state.additionalIns.phone} placeholder={'Enter contact phone number'} handleChange={this.onInputChange} />
 
-            <Input type={'email'} title={'Contact Email:'} name={'email'} value={this.state.additionalIns.email} placeholder={'Enter contact email'} handleChange={this.onInputChange} />
+           <Input type={'email'} title={'Contact Email:'} name={'email'} value={this.state.additionalIns.email} placeholder={'Enter contact email'} handleChange={this.onInputChange} />
           </div>
         )
       })
-    }else{
-      
     }
     return(
       <div>
         <h1>Insured Info</h1>
-        {newForm}
+        {knew}
       </div>
     )
   }
