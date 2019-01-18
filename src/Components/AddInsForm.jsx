200d2f1e7ff08a3b5b8cc506a6d483c6baa14767
Jon Manock <jonmanock@Jons-MacBook-Pro.local>
2019-01-18 08:06:33 -0500

    working now

--- src/Components/AddInsForm.jsx
+++ src/Components/AddInsForm.jsx
@@ -57,7 +57,23 @@ class AddInsForm extends Component{
     return(
       <div className='container'>
         <form onSubmit={this.onFormSubmit}>
+
           <Input type={'text'} title={'Interest name:'} name={'interestName'} value={this.state.Ins.interestName} placeholder={'Enter interest name'} handleChange={this.onInputChange} />
+
+          <Select title={'Relationship:'} name={'relationship'} options={['Landlord', 'Employer', 'Governmental entity','Other']} value={this.state.Ins.relationship} placeholder={'Please choose...'} handleChange={this.onInputChange} />
+
+          <Input type={'text'} title={'Address:'} name={'address'} value={this.state.Ins.address} placeholder={'Enter address:'} handleChange={this.onInputChange} />
+
+          <Select title={'State:'} name={'state'} options={this.state.options.state} handleChange={this.onInputChange} value={this.state.Ins.state} placeholder={'Please choose...'} />
+
+          <Input type={'number'} title={'Zip'} name={'zip'} value={this.state.Ins.zip} placeholder={'Enter zip'} handleChange={this.onInputChange} />
+
+          <Input type={'text'} title={'Contact name:'} name={'contactName'} value={this.state.Ins.contactName} handleChange={this.onInputChange} placeholder={'Enter contact name'} />
+
+          <Input type={'number'} title={'Contact phone:'} name={'contactPhone'} value={this.state.Ins.contactPhone} handleChange={this.onInputChange} placeholder={'Enter contact phone'} />
+
+          <Input type={'email'} title={'Contact email:'} name={'contactEmail'} value={this.state.Ins.contactEmail} handleChange={this.onInputChange} placeholder={'Enter contact email'} />
+          
           <Button type={'btn btn-success'} action={this.onFormSubmit} title={'Next'} className={'fa fa-paw'} />
         </form>
       </div>
